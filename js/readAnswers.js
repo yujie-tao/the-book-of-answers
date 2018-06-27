19a2b12d3c47a57f288d58e60a75989d6a077559
yujie-tao <yujietao@outlook.com>
2018-06-27 19:49:30 +0800

    Add answers and read answers js file

---
+++ js/readAnswers.js
@@ -0,0 +1,21 @@
+
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                console.log(allText.split("\n"));
+            }
+        }
+    }
+    rawFile.send(null);
+}
+ 
+ readTextFile("./answers.txt");
