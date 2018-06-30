2620cebf9977bdf3fb52d5f58d07249173ea1d2a
yujie-tao <yujietao@outlook.com>
2018-06-30 15:39:36 +0800

    timer with click event

--- js/timer.js
+++ js/timer.js
@@ -1,11 +1,56 @@
 var countdown = 3;
-setInterval(function() {
-  $('.second').text(countdown);
-  countdown--;
-  if (countdown < 0) {
-    $('.second').text($('#answer').text());
-    countdown = 0; 
-  }
-}, 2000);
 
-   
+setInterval(function() {
+
+  countdown--;
+  showText();
+}, 1000);
+
+
+function showText(){
+	if(countdown==2){
+	$('.second').html(countdown);
+  	$('.setence').html("[And you know the answer,</br> by heart]");
+  }
+
+   if(countdown==1){
+   	$('.second').text(countdown);
+  	$('.setence').html('[Here is the one the Book gives you,</br> are you ready?]');
+
+ //  	$( "html" ).click(function() {
+ //  	$('.second').html($('#answer').text());
+ //    $('.setence').html('');
+ //    clearInteval(countdown);
+	// });
+  }
+  // if(countdown==0){
+
+  // }
+
+  if(countdown<=0){
+
+  	$('html').click(function(){
+  		$('.second').html($('#answer').text());
+  		$('.setence').html('');
+  	})
+
+  	// document.getElementbByTagName("html").onclick=function(){
+  	// 	$('.second').html($('#answer').text());
+  	// 	$('.setence').html('');
+  	// };
+  		// $('.second').html($('#answer').text());
+  		// $('.setence').html('');
+  	// $('.second').text(1);
+  	// $('.setence').html('[Here is the one the Book gives you,</br> are you ready?]');
+  	countdown=-1;
+  }
+ 
+ //  if (countdown < 0) {
+ //  	$( "html" ).click(function() {
+ //  	$('.second').html($('#answer').text());
+ //    $('.setence').html('');
+ //    countdown=0
+	// });
+
+	// }
+}
