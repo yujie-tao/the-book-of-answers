66e9126c60c61f2315d960c272ee4625f5f000ca
yujie-tao <yujietao@outlook.com>
2018-07-02 15:21:35 +0800

    hidden horizontal scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,24 +1,21 @@
-
 $(function() {
               $('.scroll1 a').bind('click', function(event) {
+              	$("body").css({"overflow-x": "auto","max-width": "unset"});
+				$("html").css({"overflow-x": "auto","max-width": "unset"});
                 var $anchor = $(this);
-                /*
-                if you want to use one of the easing effects:
-                $('html, body').stop().animate({
-                    scrollLeft: $($anchor.attr('href')).offset().left
-                }, 1500,'easeInOutExpo');
-                 */
+                
                 $('html, body').stop().animate({
                   scrollLeft: $($anchor.attr('href')).offset().left
                 }, 1000);
                 event.preventDefault();
 
+                $("body").css({"overflow-x": "hidden"});
+              	$("html").css({"overflow-x": "hidden"});
               });
    				
             });
 
 $("#askQuestion").click(function(){
-	
     setTimeout(function(){
        $("#introtext").html("Here you have a quesiton that wish to ask </br>[It’s for serious one, not for fun] </br></br>Close your eyes</br>and medidate for several seconds</br></br>Click anywhere on the screen whenever you are ready</br>the Book will answer that for you</br>it may take couple of seconds");
        $("#introtext").addClass("fade-in");
@@ -44,9 +41,5 @@ $("#askQuestion").click(function(){
                 event.preventDefault();
 
                 timer();
-          
-                // showCounting();
-                // $('#counting').addClass('fade-in');
-
               });
             });
--- js/timer.js
+++ js/timer.js
@@ -8,10 +8,109 @@ function timer(){
   countdown = 4;
   clicked=false;
 // var clicked=false;
-  setInterval(showText1,2000);
+  setInterval(showText1,3000);
 }
 
 function showText1(){
+   countdown--;
+  if(countdown==3){
+
+   //  setTimeout(function(){
+   //    // $('.second').addClass("fade-in");
+   //    // $('.setence').addClass("fade-in");
+   //    $('.second').text(3);
+   //      $('.setence').html(" [All questions could be answered some day,</br> it’s just a matter of time]");
+   // }, 200);
+     // $('.second').addClass("fade-out");
+      // $('.setence').addClass("fade-out");
+      // $('.second').removeClass("fade-in");
+      // $('.setence').removeClass("fade-in");
+
+      setTimeout(function(){
+      $('.second').addClass("fade-in");
+      $('.setence').addClass("fade-in");
+      $('.second').text(3);
+      $('.setence').html(" [All questions could be answered some day,</br> it’s just a matter of time]");
+     
+   }, 200);
+      $('.second').removeClass("fade-in");
+      $('.setence').removeClass("fade-in");
+
+   //    setTimeout(function(){
+   //    $('.second').addClass("fade-out");
+   //    $('.setence').addClass("fade-out");
+   //    $('.second').text(3);
+   //      $('.setence').html(" [All questions could be answered some day,</br> it’s just a matter of time]");
+     
+   // }, 400);
+   //    $('.second').removeClass("fade-out");
+   //    $('.setence').removeClass("fade-out");
+
+     
+
+   //    setTimeout(function(){
+   //    $('.second').addClass("fade-out");
+   //    $('.setence').addClass("fade-out");
+     
+   // }, 400);
+
+   //    $('.second').removeClass("fade-out");
+   //    $('.setence').removeClass("fade-out");
+  }
+
+
+  if(countdown==2){
+    setTimeout(function(){
+    $('.second').addClass("fade-in");
+    $('.setence').addClass("fade-in");
+    $('.second').text(2);
+    $('.setence').html("[And you know the answer,</br> by heart]");
+   }, 200);
+    $('.second').removeClass("fade-in");
+    $('.setence').removeClass("fade-in");
+  }
+
+  if(countdown==1){
+     setTimeout(function(){
+    $('.second').addClass("fade-in");
+    $('.setence').addClass("fade-in");
+    $('.second').text(1);
+    $('.setence').html('[Here is the one the Book gives you,</br> are you ready?]');
+    // $('.instruction').html('Click anywhere to view answer');
+   }, 200);
+
+      setTimeout(function(){
+    $('.instruction').addClass("fade-in");
+    $('.instruction').html('Click anywhere to view answer');
+   }, 1000);
+
+    $('.second').removeClass("fade-in");
+    $('.setence').removeClass("fade-in");
+  }
+
+  if(countdown<=0){
+    $('#counting').click(function(){
+      if(clicked==false){
+        $(".second").html('');
+        $('.setence').html('');
+        $('.instruction').html('');
+        setTimeout(function(){
+          $(".second").html($('#answer').text());
+          $(".second").addClass("fade-in");
+   }, 700);
+       clicked=true;
+        $('.second').removeClass("fade-in");
+     }
+    })
+
+    countdown=-1;
+  }
+
+
+ 
+}
+
+function showText(){
    countdown--;
   if(countdown==3){
 
@@ -43,8 +142,14 @@ function showText1(){
     $('.setence').addClass("fade-in");
     $('.second').text(1);
     $('.setence').html('[Here is the one the Book gives you,</br> are you ready?]');
-    $('.instruction').html('Click anywhere to view answer');
+    // $('.instruction').html('Click anywhere to view answer');
    }, 200);
+
+      setTimeout(function(){
+    $('.instruction').addClass("fade-in");
+    $('.instruction').html('Click anywhere to view answer');
+   }, 800);
+
     $('.second').removeClass("fade-in");
     $('.setence').removeClass("fade-in");
   }
@@ -70,41 +175,3 @@ function showText1(){
 
  
 }
-
-function showText(){
-  countdown--;
-  if(countdown==3){
-    $('.second').html(countdown);
-    $('.setence').html(" [All questions could be answered some day,</br> it’s just a matter of time]");
-  }
-	if(countdown==2){
-	 $('.second').html(countdown);
-  	$('.setence').html("[And you know the answer,</br> by heart]");
-  }
-
-   if(countdown==1){
-   	$('.second').text(countdown);
-  	$('.setence').html('[Here is the one the Book gives you,</br> are you ready?]');
-    $('.instruction').html('Click anywhere to view answer');
-  }
-  
-
-  if(countdown<=0){
-  	$('#counting').click(function(){
-      if(clicked==false){
-        $(".second").html('');
-        $('.setence').html('');
-        $('.instruction').html('');
-        setTimeout(function(){
-          $(".second").html($('#answer').text());
-          $(".second").addClass("fade-in");
-   }, 700);
-       clicked=true;
-     }
-  	})
-
-  	countdown=-1;
-  }
-
-}
-
